d1d12539a4507cad09c77526f3034adc665d0421
Kail-225 <bobyybobin@gmail.com>
2026-02-19 23:19:59 +0300

    а

--- command.py
+++ command.py
@@ -2,6 +2,6 @@ import discord
 from boot import *
 from discord.ext import commands
 def coms(bot):
-    @bot.slash_command(name="test",description="тестовая функция")
+    @bot.slash_command(name="settings",description="Вызов меню настроек бота")
     async def test(ctx):
         return
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-import discord
+import discord,asyncio
+from boot import *
 from discord.ext import commands
 from command import coms
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
-t=access["ctc"]
+t=access()["ctc"]
+async def autoreg(guild):
+    mems=guild.members
+    for i in mems:
+        if str(i).find("(")!=-1:
+            idu=guild.get_member_named(str(i).split("(")[1][0:-1]).id
+            name=str(i).split("(")[0][0:-1]
+            add_user(idu,name,guild.id)
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}")
@@ -12,12 +20,40 @@ async def on_ready():
     print("Started without errors!")
 @bot.event
 async def on_voice_state_update(member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
+    print(f"member: {member}\nbefore:{before}\nafter:{after}")
     guild=member.guild
-    print(guild)
-    #print(f"Участник: {member}\nПеред обновлением: {before}\nПосле обновления: {after}")
-    if str(after.channel)=="Основной":
-        name_channel=str("Канал")
-        time_channel=await guild.create_voice_channel(name=name_channel)
-        await member.move_to(time_channel)
+    try:
+        #channel=check_channel(member.id,guild.id)
+        match after.channel:
+            case None:
+                if before.channel.id==925311871848947775:
+                        list=discord.VoiceChannel(925311871848947775).members
+                        print(list)
+            case _:
+                if before.channel!=None:
+                    if before.channel.id==925311871848947775:
+                        list=discord.VoiceChannel(925311871848947775).members
+                        print(list)
+                time_channel=guild.create_voice_channel(name=f"Канал {member.global_name}")
+                member.move_to(time_channel)
+    except Exception as e:
+        print(f"Ошибка: {e}")
+@bot.event
+async def on_guild_join(guild):
+    server=check_server(guild.id)
+    if server==None:
+        add_server(guild.id,guild.name)
+        await autoreg(guild)
+    else:
+        return
+@bot.event
+async def on_member_join(member: discord.Member):
+    guild=member.guild
+    user=check_user(member.id,guild.id)
+    if user==None:
+        add_user(member.id,guild.id)
+    else:
+        return
+con()
 coms(bot)
 bot.run(t)
